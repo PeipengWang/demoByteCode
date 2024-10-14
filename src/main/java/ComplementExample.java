ef111bab99d63864eef22207b3a47c9ceb8e55ea
王培鹏 <wangpeipeng123@163.com>
2024-10-14 16:08:39 +0800

    n位补码计算方法

--- src/main/java/ComplementExample.java
+++ src/main/java/ComplementExample.java
@@ -1,6 +1,9 @@
+/**
+ * 补码获取
+ */
 public class ComplementExample {
     public static void main(String[] args) {
-        int number = 2;
+        int number = -65535;
 
         // 只保留低 8 位
         int mask8Bit = 0xFF; // 8位掩码
@@ -10,5 +13,15 @@ public class ComplementExample {
         String binaryString = Integer.toBinaryString(complement8);
         System.out.println("原数: " + number);
         System.out.println("8位补码表示: " + binaryString);
+
+        int mask24Bit = 0xFFFFFF; // 24 位掩码
+
+        int complement24 = number & mask24Bit;
+
+        String binary24String = Integer.toBinaryString(complement24);
+
+        System.out.println(binary24String.length());
+        System.out.println("24位补码表示：" + binary24String);
+
     }
 }
